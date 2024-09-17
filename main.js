b9da85f475e1cf234248193de7d3adf746470548
Anders Norrback Bornholm <anders@bornholm.se>
2024-09-17 09:07:53 +0200

    Added dockerization

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ document.querySelector('#app').innerHTML = `
     <h1>Audio Visualizer Experiments</h1>
     <ul class="read-the-docs">
       <li><a href="/lab/f1/">Hyper-Dimensional Racing (official music video)</a></li>
+      <li><a href="/simple.html">Simple 2D bars</a></li>
       <li><a href="/lab/moon/">Moon (only 3d, no audio)</a></li>
-      <li><a href="/lab/moon/">Simple 2D bars</a></li>
-      <li><a href="/lab/moon/">3D bars</a></li>
-      <li><a href="/lab/moon/">3D bars + spaceship model</a></li>
-      <li><a href="/lab/moon/">3D bars + spaceship model (camera movement)</a></li>
-      <li><a href="/lab/moon/">3D bars + spaceship model (more camera movement)</a></li>
+      <li><a href="/3d-bars.html">3D bars</a></li>
+      <li><a href="/model.html">3D bars + spaceship model</a></li>
+      <li><a href="/model2.html">3D bars + spaceship model (camera movement)</a></li>
+      <li><a href="/model3.html">3D bars + spaceship model (more camera movement)</a></li>
     </ul>
   </div>
 `
